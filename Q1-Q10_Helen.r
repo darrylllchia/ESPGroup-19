2a704e45ab0395bf33ad6778cb9245ca14331ebe
Helen <some@example.com>
2024-10-02 22:52:14 +0100

    copy Q6,7,9,10 to txt

--- Q1-Q10_Helen.r
+++ Q1-Q10_Helen.r
@@ -30,28 +30,19 @@ split_punct <- function(txt,punc){
 spl_punc <- split_punct(a,c(",",".", ";", "!", ":","?")) ##over
 
 #6.
-#a
-txt <- spl_punc[nzchar(spl_punc) & !is.na(spl_punc)] #remove NA and "" from text
+txt <- spl_punc[nzchar(spl_punc) & !is.na(spl_punc)] # remove NA and "" from text
 uni_txt <- unique(tolower(txt)) # find the vector of unique words
-#b
-ii <- match(tolower(txt),uni_txt)
-#c
-freq <- tabulate(ii, nbins = length(uni_txt))
-#d
+ii <- match(tolower(txt),uni_txt) # mark words in main text with unique words index
+freq <- tabulate(ii, nbins = length(uni_txt)) # calculate word frequency
 desc <- sort(freq, decreasing = TRUE)
-# l_range <- match(desc[1000]+1,desc)
-# h_range <- match(desc[1000]-1,desc)-1
-threshold <- desc[1000]
-#e
-b <- uni_txt[which(freq >= threshold)] ##m most common words
+threshold <- desc[1000] # get the threshold frequency of common words
+b <- uni_txt[which(freq >= threshold)] # select m most common words
 
 
 # 7.
-# a
-token <- match(tolower(txt),b)
-# b
+token <- match(tolower(txt),b) #replace the common words in main text with its token
 mlag <- 4
-M <- matrix(rep(0,(length(token)-mlag)*(mlag+1)), length(token)-mlag, mlag+1)
+M <- matrix(rep(0,(length(token)-mlag)*(mlag+1)), length(token)-mlag, mlag+1) # create a zero matrix
 for (i in 1:(mlag+1)) {
     M[,i] <- token[i:(length(token)-mlag-1+i)]
 }
@@ -60,8 +51,8 @@ for (i in 1:(mlag+1)) {
 # 8.
 mlag <- 4
 nw <- 50
-token_sel <- rep(0,nw)
-token1 <- sample(token[!is.na(token)], 1)
+token_sel <- rep(0,nw) # create vector to store selected words 
+token1 <- sample(token[!is.na(token)], 1) 
 token_seq <- c(rep(NA,mlag-1),token1)
 token_sel[1] <- token1 #1st word
 for (i in 2:nw) {
@@ -83,27 +74,31 @@ cat(words_sel, sep = '\n')
 
 # 9.
 n = 50
-b_fac <- as.factor(b)
-b_num <- as.numeric(b_fac)
-uni_b_num <- unique(b_num)
-freq <- tabulate(b_num)
-prob <- freq / sum(freq)
-b_index <- sample(uni_b_num,n,replace = FALSE,prob = prob)
-word_select <- b[b_index]
-cat(word_select, sep = '\n')
+token_select <- sample(token[!is.na(token)], n, replace = TRUE) # non-return sample tokens
+word_select <- b[token_select] # find certain word to each token
+word_select
+
+# b_fac <- as.factor(b)
+# b_num <- as.numeric(b_fac)
+# uni_b_num <- unique(b_num)
+# freq <- tabulate(b_num)
+# prob <- freq / sum(freq)
+# b_index <- sample(uni_b_num,n,replace = FALSE,prob = prob)
+# word_select <- b[b_index]
+# cat(word_select, sep = '\n')
 
 
 # 10.
 txt <- spl_punc[nzchar(spl_punc) & !is.na(spl_punc)]
-position_upper <- grep('[A-Z]', txt)
-uni_txt <- unique(tolower(txt))  #unique text 不会把标点符号unique
-ii_lower <- match(txt[-position_upper],uni_txt)
-ii_upper <- match(tolower(txt[position_upper]),uni_txt)
-u_seq <- tabulate(ii_lower, nbins = length(uni_txt)) > tabulate(ii_upper,nbins = length(uni_txt))
-uni_txt[which(!u_seq)] <- paste(toupper(substr(uni_txt[which(!u_seq)],1,1)),substr(uni_txt[which(!u_seq)],2,nchar(uni_txt[which(!u_seq)])), sep = '')
+position_upper <- grep('[A-Z]', txt) # find the index of words start with capital letter in text
+uni_txt <- unique(tolower(txt))  #extract unique text
+ii_lower <- match(txt[-position_upper],uni_txt) #extract token of lower case words
+ii_upper <- match(tolower(txt[position_upper]),uni_txt) #extract token of words start with capital letter
+u_seq <- tabulate(ii_lower, nbins = length(uni_txt)) > tabulate(ii_upper,nbins = length(uni_txt)) #find the words index which often start with a capital letter in the main text
+uni_txt[which(!u_seq)] <- paste(toupper(substr(uni_txt[which(!u_seq)],1,1)),substr(uni_txt[which(!u_seq)],2,nchar(uni_txt[which(!u_seq)])), sep = '') # replace these words with words start with a capital letter
 ii <- match(tolower(txt),tolower(uni_txt))
 freq <- tabulate(ii, nbins = length(uni_txt))
 desc <- sort(freq, decreasing = TRUE)
 threshold <- desc[1000]
-b <- uni_txt[which(freq >= threshold)] ##m most common words
+b <- uni_txt[which(freq >= threshold)] # m most common words with certain words start with capital letter
 # ? space before punction? we do not have?
