9f53063b345817c9a532c2f66444e074eb32e5cb
Helen-wu-s <s2664441@ed.ac.uk>
2024-10-17 23:10:13 +0100

    Update proj2-Jiayi.r

--- proj2-Jiayi.r
+++ proj2-Jiayi.r
@@ -5,6 +5,7 @@
 
 # Aim of this task: Use simulation based method to infer fatal incidence rates from Covid deaths in English hospitals.
 
+# Approach:
 # 1. Assign each victim a guessed time of infection.
 # 2. Add a random draw from the infection-to-death distribution to each time of infection to get the implied times of death.
 # 3. Randomly propose to move each time of infection a few days, only accepting the proposed move if the change improves the simulation’s fit to the real death time distribution. 
@@ -14,30 +15,37 @@
 # (Use real death data as expected values for Poisson variables, then simulate Poisson data based on these values to replace the real data.)
 # 7. Plot a graph to illustrate the fit of the simulation model to the real death data.
 
-setwd("/Users/macbook/Statistical-Programming/assignment")
+# setwd("/Users/macbook/Statistical-Programming/assignment")
 data <- read.table('engcov.txt')[1:150, ] 
 
 t <- data$julian
 deaths <- data$nhs
 
-#'Function Name: deconv
-#'Description: This function displays the estimated death and incidence rate of covid 19 compared to the actual deaths in England. 
-#'Parameters:
-#'  t: The days to be analysed
-#'  deaths: The number of deaths on each day
-#'Output:
-#'  A vector of Ps after each iteration
-#'  A list containing a matrix(inft) of the simulated number of deaths after each iteration
-#'  The converged t0
-#'Note:
-#'  Data on daily death numbers is compared against simulated data sampled from a distribution with set parameters.
-#'  The loss function (P) involved is proportional to the squared difference between the simulated deaths and the actual deaths each day. 
-#'  The process repeats for a set number of times (n.rep). Over time, P is expected to decrease.
-#'  Bootstrapping also helps to see any uncertainty with our simulation and can be activated with the logical input to the function 'bs'.
-#'  It compares the converged vector of days of death(t0) with data generated from a poisson distribution.
-#'  Use The actual number of deaths of each day as the lambda parameter for each day.
+#' Function Name: deconv
+#' 
+#' This function displays the estimated incidence and death on each day of covid 19 based on real death data, 
+#' and quantify its uncertainty using simple bootstrapping approach.
+#' 
+#' @param t: A vector represents the days of the year to be analysed.
+#' @param deaths: A vector represents the number of deaths on each day.
+#' @param n.rep: Iteration count.
+#' @param bs: Logical. Run the bootstrapping part when it is TRUE.
+#' @param t0: Controling the calculation of guessed infection day of victims.
+#'            If NULL, calculate the guessed infection day of victims, otherwise use the known t0.
+#' @return A list containing three elements:
+#'         - P: A vector of history Ps after each iteration
+#'         - inft: A matrix(310 × n.rep) containing the simulated number of infections per day after each iteration
+#'         - t0: A vector contains final state of t0(the converged t0)
+#' @approach
+#' Real data on daily death numbers is compared against simulated data with samples from a distribution with set parameters.
+#' The loss function (P) involved is proportional to the squared difference between the simulated deaths and the actual deaths each day.
+#' Improving the simulation’s fit to the real death time distribution by moving the individuals' simulated death day to reduce P.
+#' The process iterate the set number of times (n.rep) until convergence. Over time, P is expected to decrease.
+#' Bootstrapping also helps to see any uncertainty with our simulation and can be activated with the logical input to the function 'bs'.
+#' For bootstrapping, use the actual number of deaths of each day as the expected value of poisson distribution to replace real death data.
+#' Then run the model for certain times, comparing the converged vector of days of infection(t0) with those based on generated poisson data.
 
-deconv <- function(t,deaths,n.rep=100,bs=FALSE,t0=NULL,n.times = 100){
+deconv <- function(t,deaths,n.rep=100,bs=FALSE,t0=NULL){
   death_d <- rep(t, deaths) # create a vector of death day for each individual fatalities
   n <- length(death_d) # number of fatalities
   max_day <- 310 # set the max observation day
@@ -69,7 +77,7 @@ deconv <- function(t,deaths,n.rep=100,bs=FALSE,t0=NULL,n.times = 100){
   legend("topright", legend = c('Real deaths', 'Simulated deaths', 'Estimated incidence'), col = c("black", "blue", "red"), lwd = 3) 
   
   for(k in 1:n.rep){
-    # 4 Generate n new as infection-to-death duration and add these to the infection times in t0 to get simulated death days. Compute P
+    # 4 Generate n new as infection-to-death duration and add these to the infection times in t0 to get simulated death days. Compute P.
     # generate n new draws from the infection-to-death distribution as simulation duaration
     simu_duat <- sample(c(1:80), n, replace = TRUE, prob = norm_prob_vec)
     death_d_s <- t0 + simu_duat # add simulation duration to t0 to get simulated death days
@@ -94,7 +102,7 @@ deconv <- function(t,deaths,n.rep=100,bs=FALSE,t0=NULL,n.times = 100){
     death_d_bm <- t0_dura_ran[ ,1] + t0_dura_ran[ ,2] # create death day of each individual before moving
     death_d_m <- t0_dura_ran[ ,1] + t0_dura_ran[ ,2] + moving # create death day of each individual after moving
     
-    # for each elements in t0, accept the proposed move if the change improves the simulation’s fit to the real death time distribution, ie. reduce P
+    # for each elements in t0, accept the proposed move if the change improves the simulation’s fit to the real death time distribution(ie. reduce P)
     for (i in 1:length(death_d_m)) {
       dead_bef_move <- death_d_bm[i] # the death day before move
       dead_aft_move <- death_d_m[i] # the death day after move
@@ -116,16 +124,16 @@ deconv <- function(t,deaths,n.rep=100,bs=FALSE,t0=NULL,n.times = 100){
     }
     
     t0 <- t0_dura_ran[ ,1] 
-    inci <- tabulate(t0, nbins = max_day)
-    P_vec <- c(P_vec,P)
-    inft[ ,k] <- inci
+    inci <- tabulate(t0, nbins = max_day) # calculate incidence on each day
+    P_vec <- c(P_vec,P) #store the history P
+    inft[ ,k] <- inci # store the vector of incidence on each day
     t0_mat[ ,k] <- t0
-    lines(c(1:max_day), inci, col = 'red')
-    lines(c(1:max_day), d_s, col = 'blue')
+    lines(c(1:max_day), inci, col = 'red') # add a simulated incidence line to the plot
+    lines(c(1:max_day), d_s, col = 'blue') # add a simulated deaths line to the plot
     
   }
   
-  # If bs = TRUE, run the following code to quantify its uncertainty.
+  # If bs = TRUE, run the following code to quantify the uncertainty of estimated incidence we've got.
   # Treats each real deaths data as a estimate of the expected values of a Poisson random variables.
   # Get 150 Poisson distributions and randomly select numbers from them to get real data estimates. 
   if(bs){
@@ -136,12 +144,12 @@ deconv <- function(t,deaths,n.rep=100,bs=FALSE,t0=NULL,n.times = 100){
     text(x = 84, y = -50, labels = "84", pos = 3, col = "black")
     death_simulation <- sapply(deaths, function(x) rpois(n.times, x))
     
-    for(k in 1:n.times){
+    for(k in 1:100){
       
       deaths_new <- death_simulation[k, ]
       
       if(is.null(t0)){
-        # 2
+        # 2 Guess the infection time of each victim
         #create a vector of death day for each individual fatality
         death_d <- rep(t, deaths_new)
         # randomly generate n infection-to-death durations with replacement based on probability vector
@@ -151,14 +159,14 @@ deconv <- function(t,deaths,n.rep=100,bs=FALSE,t0=NULL,n.times = 100){
       }
       
       death_d <- rep(t, deaths_new)
-      # 4 
+      # 4 Generate n new as infection-to-death duration and add these to the infection times in t0 to get simulated death days. Compute P.
       # generate n new draws from the infection-to-death distribution as simulation duaration
       simu_duat <- sample(c(1:80), n, replace = TRUE, prob = norm_prob_vec)
       death_d_s <- t0 + simu_duat # add simulation duration to t0 to get simulated death days
       d_s <- tabulate(death_d_s, nbins = max_day) # create a vector of simulated deaths on each day
       P <- sum((d - d_s)^2 / pmax(1, d_s)) # calculate modified Pearson statistic P
       
-      # 5. randomly order each time of infection and move it a few days
+      # 5 Randomly order each time of infection and move it a few days
       t0_dura <- cbind(t0, simu_duat) # combine t0 to duration time to keep infection to death duration fixed
       t0_dura_ran <- t0_dura[sample(length(t0)), ] # randomly order t0
       
@@ -176,7 +184,7 @@ deconv <- function(t,deaths,n.rep=100,bs=FALSE,t0=NULL,n.times = 100){
       death_d_bm <- t0_dura_ran[ ,1] + t0_dura_ran[ ,2] # create death day of each individual before moving
       death_d_m <- t0_dura_ran[ ,1] + t0_dura_ran[ ,2] + moving # create death day of each individual after moving
       
-      # for each elements in t0, accept the proposed move if the change improves the simulation’s fit to the real death time distribution, ie. reduce P
+      # for each elements in t0, accept the proposed move if the change improves the simulation’s fit to the real death time distribution(ie. reduce P)
       for (i in 1:length(death_d_m)) {
         dead_bef_move <- death_d_bm[i] # the death day before move
         dead_aft_move <- death_d_m[i] # the death day after move
@@ -197,8 +205,8 @@ deconv <- function(t,deaths,n.rep=100,bs=FALSE,t0=NULL,n.times = 100){
         }
       }
       
-      inci <- tabulate(t0_dura_ran[ ,1], nbins = max_day)
-      lines(c(1:max_day), inci, col = 'red')
+      inci <- tabulate(t0_dura_ran[ ,1], nbins = max_day) # calculate deaths on each day
+      lines(c(1:max_day), inci, col = 'red') # add a simulated incidence line based on bootstrapping data to the plot
       intr[ ,k] <- inci
       
     }
@@ -208,7 +216,7 @@ deconv <- function(t,deaths,n.rep=100,bs=FALSE,t0=NULL,n.times = 100){
   return(list(P = P, inft = inft, t0 = t0))
 }
 
-output <- deconv(t, deaths, n.rep = 100, bs = TRUE, t0 = NULL, n.times = 100)
+output <- deconv(t, deaths, n.rep = 100, bs = TRUE, t0 = NULL)
 output$P
 output$inft
 output$t0
