18ec4fa36c1d465ead978f3325dc295935cd6e5b
Helen <some@example.com>
2024-11-13 17:55:26 +0000

    v1

--- proj3-jiayi.r
+++ proj3-jiayi.r
@@ -1,11 +1,13 @@
 library(nlme);library(lme4)
 lmm(score ~ Machine,Machines,list("Worker",c("Worker","Machine")))
+exp(lmm(score ~ Machine,Machines,list("Worker",c("Worker","Machine")))$theta)
 lmer(score ~ Machine + (1|Worker) + (1|Worker:Machine),data=Machines,
 REML=FALSE)
 
 form <- score ~ Machine
 dat <- Machines
 ref <- list("Worker",c("Worker","Machine"))
+
 
 LMMsetup <- function(form, dat, ref){
     # set up Z, X and other things that need to be setup only o
@@ -20,18 +22,20 @@ LMMsetup <- function(form, dat, ref){
 
     # set up Z
     Z <- matrix(nrow = dim(df)[1], ncol = 0)
-    for(i in length(ref)){
+    f <- c()
+    for(i in 1:length(ref)){
         # paste each element in ref to get a string in formula format
         fomula <- paste('~', paste(paste(ref[[i]], collapse = ':'), '-1', sep = ''), sep = '')
-
+        
+        model <- model.matrix(as.formula(fomula), df)
         # combine columns of model matrix of each block together
-        Z <- cbind(Z, model.matrix(as.formula(fomula), df))
+        Z <- cbind(Z, model)
+        
+        f <- c(f, ncol(model))
     }
 
     # set up sigma 
-    # reg <- lm(form, df)
-    # sigma <- sqrt(sum(reg$residuals^2) / (reg$df.residual+1))
-    return(list(X = X, Z = Z, y = y, sigma = sigma))
+    return(list(X = X, Z = Z, y = y, f = f))
 
 }
 
@@ -46,47 +50,48 @@ LMMprof <- function(form, dat, ref, theta){
     Z <- result$Z
     X <- result$X
     y <- result$y
+    f <- result$f
+    f_num <- sum(f)
     n <- dim(X)[1]
     p <- dim(X)[2]
-
+    
+    
     # QR decompose Z
     qrz <- qr(Z)
     R <- qr.R(qrz)
-
+    
+    # extend theta
+    # theta <- c(log(sigma), log()) it doesn't matter what theta is
+    sigma <- exp(theta[1])
     # calculate first term of RHS of w
-    first_term <- R %*% t(R) * rep(exp(theta)^2, p) + diag(sigma^2, p)
+    first_term <- R %*% t(R) * rep(exp(theta[2:(length(ref)+1)])^2, f) + diag(sigma^2, f_num)
 
     # decompose first_term using Cholesky decomposition
     S <- chol(first_term)
     qty <- qr.qty(qrz, y)
     qtx <- qr.qty(qrz, X)
-    M <- forwardsolve(t(S), qty[1:p,])
-    N <- forwardsolve(t(S), qtx[1:p,])
+    M <- forwardsolve(t(S), qty[1:f_num])
+    N <- forwardsolve(t(S), qtx[1:f_num, ])
 
     # calculate XTWy and XTWX
-    XTWy <- rbind(t(N) %*% M, t(qtx[(p+1):n,]) %*% qty[(p+1):n,] * sigma^2)
-    XTWX <- rbind(t(N) %*% N, t(qtx[(p+1):n,]) %*% qtx[(p+1):n,] * sigma^2)
+    XTWy <- t(N) %*% M + t(qtx[(f_num+1):n, ]) %*% qty[(f_num+1):n] * sigma^2
+    XTWX <- t(N) %*% N + t(qtx[(f_num+1):n, ]) %*% qtx[(f_num+1):n, ] * sigma^2
 
     # decompose XTWX with Cholesky decomposition
     l <- chol(XTWX)
-
-    beta_hat <- function(form, dat, ref, theta){
-        beta_hat <- backwardsolve(l, forwardsolve(t(l), XTWy))
-        beta_l <- c(beta_l, list(bata_hat))
-        return(bata_hat)
-    }
+    
+    beta_hat <- backsolve(l, forwardsolve(t(l), XTWy))
 
     # calcualte log likelihood
-    qtyx <- qr.qty(qrz, y - X%*%bata_hat)
-    P <- forwardsolve(t(S), qtyx[1:p,])
+    qtyx <- qr.qty(qrz, y - X%*%beta_hat)
+    P <- forwardsolve(t(S), qtyx[1:f_num])
 
-    neg_loglike <- function(form, dat, ref, theta){
-        loglike <- rbind(t(P) %*% P, t(qtyx[(p+1):n,]) %*% qtyx[(p+1):n,] * sigma^2)/
-        (sigma^2) / 2 + sum(log(diag(S))) + (n-p)*log(sigma)
-        return(loglike)3
-    }
-
-    return(list(neg_loglike = neg_loglike, beta_hat = beta_hat))
+    loglike <- (t(P) %*% P + t(qtyx[(f_num+1):n]) %*% qtyx[(f_num+1):n] * sigma^2)/
+    (sigma^2) / 2 + sum(log(diag(S))) + (n-p)*log(sigma)
+    
+    attr(loglike, 'beta_hat') <- beta_hat
+    
+    return(loglike)
 }
 
 
@@ -98,17 +103,15 @@ lmm <- function(form,dat,ref=list()){
     # form: a model formula
     # dat: the data frame containing all the variables needed in the model
     # ref: a list of vectors of variable names specifying random effects for Zb part of the model
-    theta_l <- list()
-    beta_l <- list()
-    tar_fun <- function(form, dat, ref, theta){
-        neg_loglike <- LMMprof(form, dat, ref, theta)$neg_loglike
-        attr(neg_loglike, 'beta_hat') <- LMMprof(form, dat, ref, theta)$beta_hat
-        theta_l <- c(params, list(par))
-        beta_l <- c(beta_l, list(beta_hat))
-    }
-    sigma <- LMMsetup(form,dat,ref)$sigma
-    fit <- optim(log(sigma), LMMprof, form = form, dat = dat, ref = ref)
-    return(list(beta = beta_l, theta = theta_l))
+    block_num <- length(ref)
+    theta_start <- rep(0, length(ref)+1)
+    
+    fit <- optim(theta_start, LMMprof, form = form, dat = dat, ref = ref)
+    
+    theta <- fit$par
+    beta_hat <- attr(LMMprof(form, dat, ref, theta), "beta_hat")
+    
+    return(list(theta = theta, beta_hat = beta_hat))
 }
 
 
