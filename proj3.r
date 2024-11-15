13f20bc7246f117255a227e63a84abfbf45930ed
darrylllchia <100289348+darrylllchia@users.noreply.github.com>
2024-11-15 01:17:03 +0000

    Update proj3.r

--- proj3.r
+++ proj3.r
@@ -15,7 +15,7 @@
 LMMsetup <- function(form, dat, ref){
   #' The function is to set up our fixed effect and random effect matrices X and Z, along with the y vector. 
   #' The vector f contains the number of columns of each block in each term in ref. 
-
+  
   # Convert the input data into a data frame format
   df <- data.frame(dat) 
   
@@ -27,33 +27,33 @@ LMMsetup <- function(form, dat, ref){
   
   # Set up Z (Random effects)
   Z <- matrix(nrow = dim(df)[1], ncol = 0)
-
+  
   # Initilise vector f to store the number of columns of each block
   f <- c()
-
+  
   # Update random effect matrix Z if we have random effect
   if (length(ref) > 0){
     for (i in 1:length(ref)){
-
+      
       # Convert random effect into formula format
       fomula <- paste('~', paste(paste(ref[[i]], collapse = ':'), '-1', sep = ''), sep = '')
       # Set up model matrix
       model <- model.matrix(as.formula(fomula), df)
-
+      
       # Combine model matrix of each block together
       Z <- cbind(Z, model)
       # Number of columns of each block
       f <- c(f, ncol(model))
     }
   }
-
+  
   return(list(X = X, Z = Z, y = y, f = f))
   
 }
 
 
 
-LMMprof <- function(form, dat, ref, theta){
+LMMprof <- function(theta,X,Z,y,f){
   #'
   #' Evaluate the (negative) log likelihood for a given theta
   #' Compute the corresponding beta_hat
@@ -61,50 +61,44 @@ LMMprof <- function(form, dat, ref, theta){
   #' @param theta: vector of log(std_devs). First element of theta is the log(std_dev) of residuals.
   #'               Subsequent elements correspond to the log(std_devs) of each block in Z.
   #' @return negative log likelihood function for optim to minimise and the calculated beta_hat vector given a theta vector.
-
-  # Set up Z, X, y and f with LMMsetup function
-  result <- LMMsetup(form, dat, ref)
-  Z <- result$Z
-  X <- result$X
-  y <- result$y
-  f <- result$f
+  
   # Number of columns in Z matrix
   p <- sum(f)
   # Number of observations
   n <- dim(X)[1]
   # Number of fixed effects
   p1 <- dim(X)[2]
-
+  
   # Set up sigma
   sigma <- exp(theta[1])
-
+  
   # If there is no random effect
   if (length(f) == 0) {
-    # Compute beta_hat and negetive loglikelihood function
+    # Compute beta_hat and negative log likelihood function
     qrx <- qr(X)
     beta_hat <- backsolve(qr.R(qrx),qr.qty(qrx,y)[1:p1])
     neg_loglike <- t(y - X %*% beta_hat) %*% (y - X %*% beta_hat) /
       (2*sigma^2) + n*log(sigma)
   }
-
+  
   # If there is random effect
   else {
     # theta = c(log(sigma), ...) each block have the same standard deviation
     # Extend theta to diagonal of psi
     ex_theta <- rep(exp(theta[-1])^2, f)
-
+    
     # QR decompose Z
     qrz <- qr(Z)
     R <- qr.R(qrz)
-
-    # Calculate first term of W
+    
+    # Calculate first term of W (R(psi)R^{T} + sigma^2)
     first_term <- R %*% (ex_theta * t(R)) + diag(sigma^2, p)
     
     # Decompose first_term using Cholesky decomposition
     S <- chol(first_term)
     qty <- qr.qty(qrz, y)
     qtx <- qr.qty(qrz, X)
-
+    
     M <- forwardsolve(t(S), qty[1:p]) # first p row of S^{-T}Q^{T}y
     N <- forwardsolve(t(S), qtx[1:p, ]) # first p row of S^{-T}Q^{T}X
     
@@ -117,17 +111,17 @@ LMMprof <- function(form, dat, ref, theta){
     
     beta_hat <- backsolve(l, forwardsolve(t(l), XTWy))
     
-    # Calcualte log likelihood
+    # Calculate log likelihood
     qtyx <- qr.qty(qrz, y - X %*% beta_hat)
     P <- forwardsolve(t(S), qtyx[1:p]) # first p row of S^{-T}Q^{T}(y-X*beta)
-
+    
     neg_loglike <- (sum(P^2) + t(qtyx[(p+1):n]) %*% qtyx[(p+1):n] / sigma^2)/2 +
       sum(log(diag(S))) + (n-p)*log(sigma)
   }
-
+  
   # Create beta_hat as attribute of negative log likelihood
   attr(neg_loglike, 'beta_hat') <- beta_hat
-
+  
   return(neg_loglike)
 }
 
@@ -146,23 +140,23 @@ lmm <- function(form, dat, ref=list()) {
   #'         - beta: the MLEs in beta
   #' @approach "BFGS" method to optimize linear model
   #'           "Nelder-Mead" method to optimize linear mixed model
-
+  
   # Set start point of theta
   theta_start <- rep(1, length(ref)+1)
-
-  # Set up X
+  
+  # Set up Z, X, y and f with LMMsetup function
   setup <- LMMsetup(form, dat, ref)
   X <- setup$X
   n <- dim(X)[1]
-
+  
   # Find the max df in linear and linear mixed model
   p <- max(sum(setup$f), dim(X)[2])
-
+  
   # If p > n, can not use this method
   if (p > n) {
     return("Unable to run, number of columns more than number of observations")
   }
-
+  
   # If n > p, use the method
   else {
     # If there is no random effect, use BFGS method
@@ -171,16 +165,16 @@ lmm <- function(form, dat, ref=list()) {
     else {method = "Nelder-Mead"}
     
     # Optimization
-    fit <- optim(theta_start, LMMprof, form = form, dat = dat, ref = ref, 
-                  method = method)
-
+    fit <- optim(par = theta_start, fn = LMMprof, X=setup$X, Z=setup$Z, y=setup$y, f=setup$f, 
+                 method = method)
+    
     # Get optimized theta and beta_hat
     theta <- fit$par
-    beta_hat <- attr(LMMprof(form, dat, ref, theta), "beta_hat")
+    beta_hat <- attr(LMMprof(X=setup$X, Z=setup$Z, y=setup$y, f=setup$f , theta), "beta_hat")
     
     return(list(theta = theta, beta_hat = beta_hat))
   }
-
+  
 }
 
 
